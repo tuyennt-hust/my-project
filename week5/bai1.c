c4efa2e5369c08bffdb3cbd2ac9a35d6ca0d0fd3
Thanh Tuyen <thanhtuyen8102006@gmail.com>
2026-03-27 13:21:26 +0700

    update

--- week5/bai1.c
+++ week5/bai1.c
@@ -1,57 +1,24 @@
 #include <stdio.h>
-#define N 1000
-
-int A[N];
-
-void merge(int first, int mid, int last){
-    int i = first, j = mid + 1, k = first;
-    int t[N];
-
-    while(i <= mid && j <= last){
-        if(A[i] < A[j]) 
-            t[k++] = A[i++];
-        else 
-            t[k++] = A[j++];
+#define N 21
+int x[N];
+int n;
+void printSolution(){
+    for(int k = 1; k <= n; k++){
+        printf("%d",x[k]);
     }
-
-    while(i <= mid) 
-        t[k++] = A[i++];
-
-    while(j <= last) 
-        t[k++] = A[j++];
-
-    for(k = first; k <= last; k++){
-        A[k] = t[k];
-    }
+    printf("\n");
 }
 
-void merge_sort(int first, int last){
-    if(first >= last) return;
-
-    int mid = (first + last) / 2;
-
-    merge_sort(first, mid);
-    merge_sort(mid + 1, last);
-
-    merge(first, mid, last);
+void Try(int k){
+    for(int v = 0; v <= 1; v++){
+        x[k] = v;
+        if(k == n) printSolution();
+        else Try(k+1);
+    }
 }
 
 int main(){
-    int n, i;
-
-    printf("Nhap n: ");
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++){
-        scanf("%d", &A[i]);
-    }
-
-    merge_sort(0, n - 1);
-
-    printf("Mang sau khi sap xep:\n");
-    for(i = 0; i < n; i++){
-        printf("%d ", A[i]);
-    }
-
-    return 0;
+   scanf("%d",&n);
+   Try(1);
+   return 0;
 }
