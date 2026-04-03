13bcc9466e5d71380a328a482286a95522bdb807
Thanh Tuyen <thanhtuyen8102006@gmail.com>
2026-04-03 14:48:48 +0700

    update

--- week6/bai1.c
+++ week6/bai1.c
@@ -1,58 +1,81 @@
 #include <stdio.h>
 
-int grid[9][9];
-int count = 0;
+int A[9][9]; 
+int markR[9][10];
+int markC[9][10];
+int markS[3][3][10];
+int cnt;
 
-int isValid(int row, int col, int num) {
-    for (int i = 0; i < 9; i++) {
-        if (grid[row][i] == num) return 0;
-    }
-
-    for (int i = 0; i < 9; i++) {
-        if (grid[i][col] == num) return 0;
-    }
-
-    int startRow = row - row % 3;
-    int startCol = col - col % 3;
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (grid[startRow + i][startCol + j] == num)
-                return 0;
-        }
-    }
-
+int check(int v, int r, int c){
+    if(markR[r][v]) return 0;
+    if(markC[c][v]) return 0;
+    if(markS[r/3][c/3][v]) return 0;
     return 1;
 }
 
-void solve() {
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
+void Try(int r, int c){
+    if(A[r][c] > 0){
+        if(r == 8 && c == 8) cnt++;
+        else{
+            if(c == 8) Try(r+1, 0);
+            else Try(r, c+1);
+        }
+        return; 
+    }
 
-            if (grid[row][col] == 0) {
-                for (int num = 1; num <= 9; num++) {
-                    if (isValid(row, col, num)) {
-                        grid[row][col] = num;
-                        solve();
-                        grid[row][col] = 0; 
-                    }
-                }
-                return; 
+    for(int v = 1; v <= 9; v++){
+        if(check(v,r,c)){
+            A[r][c] = v;
+            markR[r][v] = 1;
+            markC[c][v] = 1;
+            markS[r/3][c/3][v] = 1;
+
+            if(r == 8 && c == 8) cnt++;
+            else{
+                if(c == 8) Try(r+1, 0);
+                else Try(r, c+1);
+            }
+
+            markR[r][v] = 0;
+            markC[c][v] = 0;
+            markS[r/3][c/3][v] = 0;
+            A[r][c] = 0;
+        }
+    }
+}
+
+void init(){
+    for(int v = 1; v <= 9; v++){
+        for(int r = 0; r < 9; r++) 
+            markR[r][v] = 0;
+        for(int c = 0; c < 9; c++) 
+            markC[c][v] = 0;
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                markS[i][j][v] = 0;
+    }
+}
+
+int main(){
+    init();
+
+    for(int r = 0; r < 9; r++){
+        for(int c = 0; c < 9; c++){
+            int v;
+            scanf("%d", &v);
+            A[r][c] = v;
+
+            if(v > 0){
+                markR[r][v] = 1; 
+                markC[c][v] = 1; 
+                markS[r/3][c/3][v] = 1;
             }
         }
     }
-    count++;
-}
 
-int main() {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            scanf("%d", &grid[i][j]);
-        }
-    }
+    cnt = 0;
+    Try(0,0);
 
-    solve();
-
-    printf("%d\n", count);
+    printf("%d\n", cnt);
     return 0;
 }
